a04149787832b92a9512e5ee2d37e8a7c72a06fb
GeneTv <contact@genetv.net>
2021-12-14 08:47:11 +0100

    Added multiplayer service temporary

--- app/src/main/java/io/genetv/rockpaperscissors/serivce/MultiplayerService.java
+++ app/src/main/java/io/genetv/rockpaperscissors/serivce/MultiplayerService.java
@@ -1,6 +1,28 @@
 package io.genetv.rockpaperscissors.serivce;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+
 public class MultiplayerService {
 
+    private MultiplayerService instance;
+    private BluetoothAdapter bluetoothAdapter;
 
+    private MultiplayerService() {
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    }
+
+    public MultiplayerService getInstance() {
+        if(instance == null)
+            instance = new MultiplayerService();
+        return instance;
+    }
+
+    public void startOpponentSearch() {
+
+    }
+
+    public void pairDevice(BluetoothDevice device) {
+        
+    }
 }
