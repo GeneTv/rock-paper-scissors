2fe8dc21bcf926fda0bd35f4da6f54d65c834089
GeneTv <contact@genetv.net>
2021-12-14 11:29:24 +0100

    Implemented game logic concept

--- app/src/main/java/io/genetv/rockpaperscissors/serivce/MultiplayerService.java
+++ app/src/main/java/io/genetv/rockpaperscissors/serivce/MultiplayerService.java
@@ -6,6 +6,12 @@ public class MultiplayerService implements IGameService {
 
     @Override
     public void makeMove(GameMove move) {
-        // TODO: Contact the game service
+        /* TODO:
+            -> Send my move to the opponent device (save it there)
+            -> Save my move to the match object
+            if(match.getOutcome() != null)
+                Transition to the game over view... Game is over since both outcomes are known
+         */
+
     }
 }
