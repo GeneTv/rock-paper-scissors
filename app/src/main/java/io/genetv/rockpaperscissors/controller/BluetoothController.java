b8d098bb10bb9f03f734ac463c0cd1e20c82d0b8
GeneTv <contact@genetv.net>
2021-12-14 08:52:44 +0100

    Removed unused Multiplayer Service

--- app/src/main/java/io/genetv/rockpaperscissors/controller/BluetoothController.java
+++ app/src/main/java/io/genetv/rockpaperscissors/controller/BluetoothController.java
@@ -1,6 +1,29 @@
 package io.genetv.rockpaperscissors.controller;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+
 public class BluetoothController {
 
-    
+    private BluetoothController instance;
+    private BluetoothAdapter bluetoothAdapter;
+
+    private BluetoothController() {
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    }
+
+    public BluetoothController getInstance() {
+        if(instance == null)
+            instance = new BluetoothController();
+        return instance;
+    }
+
+    public void startOpponentSearch() {
+
+    }
+
+    public void pairDevice(BluetoothDevice device) {
+
+    }
+
 }
